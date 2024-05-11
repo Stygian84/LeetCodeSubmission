106269402da672f41ea5bf9fcfc79e62731c9e15
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-11 09:18:49 +0800

    Time: 95 ms (95.35%) | Memory: 17.4 MB (57.95%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0
+        right=len(numbers)-1
+        while left<=right:
+            if numbers[left]+numbers[right]>target:
+                right-=1
+            elif numbers[left]+numbers[right]<target:
+                left+=1
+            else:
+                return [left+1,right+1]
+        '''
         value_needed=math.inf
         idx=0
         for item in numbers:
@@ -7,4 +17,5 @@ class Solution:
             if value_needed in numbers:
                 break
             idx+=1
-        return [idx+1,len(numbers) - 1 - numbers[::-1].index(value_needed)+1]        
+        return [idx+1,len(numbers) - 1 - numbers[::-1].index(value_needed)+1]   
+        '''     
