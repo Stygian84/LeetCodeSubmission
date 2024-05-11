5a6049e6d7319375fe06b8350c514984c6be8d07
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-12 00:31:06 +0800

    Time: 297 ms (44.02%) | Memory: 63.4 MB (81.24%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -2,24 +2,27 @@ class RandomizedSet:
 
     def __init__(self):
         self.dict={}
+        self.ls=[]
 
     def insert(self, val: int) -> bool:
         if val in self.dict:
             return False
         else:
             self.dict[val]=0
+            self.ls.append(val)
             return True
 
     def remove(self, val: int) -> bool:
         if val in self.dict:
             self.dict.pop(val)
+            self.ls.remove(val)
             return True
         else:
             return False
         
 
     def getRandom(self) -> int:
-        return random.choice(list(self.dict.keys()))
+        return random.choice(self.ls)
 
 
 
