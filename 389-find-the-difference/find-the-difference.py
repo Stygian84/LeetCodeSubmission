0180e3618d51b58ef9555aff5c2b4311e29b8e1d
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-12 00:44:16 +0800

    Time: 30 ms (93.97%) | Memory: 16.7 MB (36.03%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -2,9 +2,14 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         if len(t)==1:
             return t
+            '''
         sorted_s=''.join(sorted(s))
         sorted_t=''.join(sorted(t))
         for idx in range(len(sorted_s)):
             if sorted_s[idx]!=sorted_t[idx]:
                 return sorted_t[idx]
         return sorted_t[-1]
+        '''
+        for letter in t:
+            if s.count(letter)!=t.count(letter):
+                return letter
