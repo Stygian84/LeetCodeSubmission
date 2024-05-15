fa40718383b559970a0fb6a984b972e870b28490
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-15 15:17:02 +0800

    Time: 56 ms (33.55%) | Memory: 16.8 MB (44.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,5 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dc={}
+        for i in range(len(nums2)):
+            if nums2[i+1:]==[]:
+                dc[nums2[i]]=-1
+                continue
+            for item in nums2[i+1:]:
+                if item>nums2[i]:
+                    dc[nums2[i]]=item
+                    break
+        result=[]
+        for item in nums1:
+            result.append(dc.get(item,-1))
+        return result
+        '''
         result=[]
         for i in range(len(nums1)):
             for idx in range(len(nums2)):
@@ -15,3 +29,4 @@ class Solution:
                             break  
                     result.append(max_num)
         return result
+        '''
