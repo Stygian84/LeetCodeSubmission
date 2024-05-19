33eb61266e4005309f400e9c99a4bb3bcfc59883
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-19 15:27:14 +0800

    Time: 49 ms (43.33%) | Memory: 17.7 MB (7.95%) - LeetSync

---
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.py
@@ -0,0 +1,11 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if (start<destination):
+            pass
+
+        else:
+            start,destination=destination,start
+
+        return min(sum(distance[start:destination]),sum(distance[:start])+sum(distance[destination:]))
+        
+            
