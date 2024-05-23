321c5307a28ce2761767386622f5f36cc2c0f3ec
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 15:29:00 +0800

    Time: 30 ms (87.75%) | Memory: 16.4 MB (99.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ls=[]
+        if len(s)<=1:
+            return False
+        dc={")":"(","]":"[","}":"{"}
+        for item in s:
+            if item in dc.keys():
+                if ls and ls[-1]==dc[item]:
+                    ls.pop() 
+                else:
+                    ls.append(item)               
+            else:
+                ls.append(item)
+        print(ls)
+        if ls == []:
+            return True
+        else:
+            return False 
+        
+        
