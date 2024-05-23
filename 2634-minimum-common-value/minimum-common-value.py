bb882db338426e46269ebb0b3a70c97701924717
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 15:03:05 +0800

    Time: 382 ms (10.35%) | Memory: 40.2 MB (25.44%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,9 +1,11 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        common_elements = set(nums1) & set(nums2)
-        ls=sorted(common_elements)
-        if common_elements:
-            return ls[0]
-        else:
-            return -1
+        nums1=list(set(nums1))
+        nums2=list(set(nums2))
+        nums1.extend(nums2)
+        nums1.sort()
+        for idx in range(len(nums1)-1):
+            if nums1[idx]==nums1[idx+1]:
+                return nums1[idx]
+        return -1
         
