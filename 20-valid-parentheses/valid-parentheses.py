4eb7c41987dbecf76ca3a555692b3643b3df75d0
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 15:29:31 +0800

    Time: 30 ms (87.75%) | Memory: 16.5 MB (74.52%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -12,7 +12,6 @@ class Solution:
                     ls.append(item)               
             else:
                 ls.append(item)
-        print(ls)
         if ls == []:
             return True
         else:
