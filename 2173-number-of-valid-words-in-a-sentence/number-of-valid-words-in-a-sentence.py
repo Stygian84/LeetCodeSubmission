704b10ac4081519264995b5e252171a08c328f80
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 16:59:48 +0800

    Time: 39 ms (85.71%) | Memory: 16.7 MB (24.01%) - LeetSync

---
+++ 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countValidWords(self, sentence: str) -> int:
+        tokens=sentence.split()
+        def isValid(word):
+            hypen_count=0
+            punctuations="!,."
+            for i in range(len(word)):
+                if word[i].isdigit():
+                    return False
+                elif i!=len(word)-1 and word[i] in punctuations:
+                    return False
+                elif word[i]=="-":
+                    hypen_count+=1
+                    if hypen_count>1:
+                        return False
+                    elif i==0 or i==len(word)-1:
+                        return False
+                    elif (not word[i-1].islower()) or (not word[i+1].islower()):
+                        return False
+                elif not word[i].islower() and i!=len(word)-1:
+                    return False
+            print(word)
+            return True
+        count=0
+        for item in tokens:
+            if isValid(item):
+                count+=1
+        return count
+                
