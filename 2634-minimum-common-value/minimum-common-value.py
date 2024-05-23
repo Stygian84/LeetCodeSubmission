b1ffce1b2facb63ae6072bc0c1e044ed8335c899
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 15:05:53 +0800

    Time: 1978 ms (5.02%) | Memory: 35.6 MB (70.30%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,5 +1,6 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        '''
         nums1=list(set(nums1))
         nums2=list(set(nums2))
         nums1.extend(nums2)
@@ -8,4 +9,13 @@ class Solution:
             if nums1[idx]==nums1[idx+1]:
                 return nums1[idx]
         return -1
-        
+        '''
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums2[j]>nums1[i]:
+                    break
+                if nums2[j]==nums1[i]:
+                    return nums2[j]
+            else:
+                continue
+        return -1
