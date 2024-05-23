3bfe6dc0ad685c005e84adbc86c3afba964664d3
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-23 19:17:23 +0800

    Time: 513 ms (52.82%) | Memory: 18.4 MB (30.96%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,6 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        ls=[-1]*len(arr)
-        a=copy.deepcopy(arr)
-        a.sort()
-        for i in range(len(arr)-1):
-            a.remove(arr[i])
-            ls[i]=a[-1]
-        return ls
+        res = [-1]
+        for i in range(len(arr)-1,0,-1):
+            res.append(max(res[-1],arr[i]))
+        return res[::-1]
