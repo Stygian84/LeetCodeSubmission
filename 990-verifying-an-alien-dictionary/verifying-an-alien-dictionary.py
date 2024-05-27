0783ca6230da0a9d24a5fa306a091e77ba41fcd1
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-27 12:04:10 +0800

    Time: 38 ms (74.83%) | Memory: 16.5 MB (75.02%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        dc={}
+        for i in range(len(order)):
+            dc[order[i]]=i
+        
+        def check_word(item1,item2,dc):
+            length=min(len(item1),len(item2))
+            for i in range(length):
+                index1=dc[item1[i]]
+                index2=dc[item2[i]]
+                print(item1[i],index1)
+                print(item2[i],index2)
+                if index2<index1:
+                    return False
+                else:
+                    if item1[i]!=item2[i]:
+                        return True
+            if len(item1)>length:
+                return False
+            return True
+
+        for idx in range(len(words)-1):
+            item1=words[idx]
+            item2=words[idx+1]
+            if check_word(item1,item2,dc)==False:
+                return False
+        return True
