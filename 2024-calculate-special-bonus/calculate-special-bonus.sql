93cf6c7e293e7110afd36ad0e4226e129df2ba9a
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-29 17:33:38 +0800

    Time: 1044 ms (82.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select employee_id,
+    case 
+    when mod(employee_id,2)=1 and substr(name,1,1)!='M' then salary
+    else 0
+    end as bonus
+from employees
+order by employee_id
