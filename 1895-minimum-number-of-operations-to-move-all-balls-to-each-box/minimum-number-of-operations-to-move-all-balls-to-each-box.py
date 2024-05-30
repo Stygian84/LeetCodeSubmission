20474ca24976f63627bf53c302015f56ed62c9bc
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-30 18:06:17 +0800

    Time: 55 ms (85.21%) | Memory: 16.8 MB (88.64%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        
+        '''
         res=[0]*len(boxes)
         for idx in range(len(res)):
             count=0
@@ -9,4 +9,23 @@ class Solution:
                     count+=abs(idx-i)
 
             res[idx]=count
+        return res
+        '''
+        n = len(boxes)
+        res = [0] * n
+        
+        count, total = 0, 0
+        for i in range(n):
+            total += count
+            res[i] = total
+            if boxes[i] == '1':
+                count += 1
+        
+        count, total = 0, 0
+        for i in range(n-1, -1, -1):
+            total += count
+            res[i] += total
+            if boxes[i] == '1':
+                count += 1
+                
         return res
