736ecf77ba3d00b7a21431e67450c164292f1bee
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-05-31 11:56:23 +0800

    Time: 57 ms (63.12%) | Memory: 18.5 MB (60.05%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,5 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        '''
         dc={}
         for item in nums:
             dc[item]=dc.get(item,0)+1
@@ -8,3 +9,22 @@ class Solution:
             if values==1:
                 res.append(key)
         return res
+        '''
+        # Step 1: Calculate XOR of all elements
+        xor_val = 0
+        for num in nums:
+            xor_val ^= num
+        
+        # Step 2: Find the rightmost set bit of xor_val
+        rightmost_set_bit = xor_val & -xor_val
+        
+        # Step 3: Partition elements into two groups based on the rightmost set bit
+        unique1 = 0
+        unique2 = 0
+        for num in nums:
+            if num & rightmost_set_bit:
+                unique1 ^= num
+            else:
+                unique2 ^= num
+        
+        return [unique1, unique2]
