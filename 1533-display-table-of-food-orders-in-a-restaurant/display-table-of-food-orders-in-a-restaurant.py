9eb71384cabcd13189a78e8ab238b1c42e961e9f
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-02 16:48:41 +0800

    Time: 318 ms (76.50%) | Memory: 27.3 MB (74.50%) - LeetSync

---
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.py
@@ -0,0 +1,27 @@
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        dc={}
+        menu=set()
+        table=set()
+        for item in orders:
+            dc[item[1]]=dc.get(item[1],{})
+            dc[item[1]][item[2]]=dc[item[1]].get(item[2],0)+1
+            table.add(int(item[1]))
+            menu.add(item[2])
+        first_row=["Table"]
+        menu=list(menu)
+        menu.sort()
+        table=list(table)
+        table.sort()
+
+        for item in menu:
+            first_row.append(item)
+
+        res=[first_row]
+        for idx in table:
+            temp_ls=[str(idx)]
+            for item in menu:
+                temp_ls.append(str(dc[str(idx)].get(item,"0")))
+            res.append(temp_ls)
+
+        return res
