5875f309935aa92a5663a4a1347db8d03542706c
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-04 18:24:34 +0800

    Time: 120 ms (22.57%) | Memory: 19 MB (91.24%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dc={}
+        res=0
         for item in nums:
-            dc[item]=dc.get(item,0)+1
-        for key,values in dc.items():
-            if values==1:
-                return key
+            res^=item
+        return res
