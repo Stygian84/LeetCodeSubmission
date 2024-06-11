df186fd4676ab2b67ad6f66d36bd3a10b6753e35
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-11 17:22:57 +0800

    Time: 966 ms (95.79%) | Memory: 27.3 MB (13.37%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        
+        total=mean*(len(rolls)+n)-sum(rolls)
+        print(total)
+        if total/n>6 or total/n<1:
+            return []
+
+
+        base=total//n
+        remainder=total%n
+        value_of_max_extra=remainder//n+1
+        number_of_max_extra=remainder%n
+
+        res=[total//n+value_of_max_extra]*number_of_max_extra + [total//n]*(n-number_of_max_extra)
+
+        
+        return res
