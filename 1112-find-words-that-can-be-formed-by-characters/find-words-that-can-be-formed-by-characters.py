f97639582785040f4079c1f0506a20f0e388733f
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-21 15:31:11 +0800

    Time: 291 ms (5.97%) | Memory: 17.1 MB (83.00%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        dc={}
+        for item in chars:
+            dc[item]=dc.get(item,0)+1
+
+        total=0
+        for word in words:
+            dc2=copy.deepcopy(dc)
+            for letter in word:
+                if dc2.get(letter,0)==0:
+                    break
+                else:
+                    dc2[letter]-=1
+            else:
+                total+=len(word)
+        return total
