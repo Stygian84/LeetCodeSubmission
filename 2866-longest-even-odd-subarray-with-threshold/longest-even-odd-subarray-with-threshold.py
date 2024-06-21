0051acda577a60f3eadeb008cd036595b0ab81ed
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-21 11:56:39 +0800

    Time: 261 ms (57.23%) | Memory: 16.5 MB (98.59%) - LeetSync

--- 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.py
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.py
@@ -1,20 +1,27 @@
 class Solution:
     def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
-        window=[]
 
-        longest_window=0
+        window = []
+        longest_window = 0
+
         for item in nums:
-            longest_window=max(len(window),longest_window)
-            if len(window)==0 and item%2==0 and item<=threshold:
+            longest_window = max(len(window), longest_window)
+
+            #leftmost element
+            if len(window) == 0 and item % 2 == 0 and item <= threshold:
                 window.append(item)
                 continue
-            if window and window[-1]%2!=item%2 and item<=threshold:
+
+            if window and window[-1] % 2 != item % 2 and item <= threshold:
                 window.append(item)
                 continue
             else:
-                window=[]
-                if len(window)==0 and item%2==0 and item<=threshold:
+                window = []
+                #leftmost element
+                if len(window) == 0 and item % 2 == 0 and item <= threshold:
                     window.append(item)
                     continue
-        longest_window=max(len(window),longest_window)
+                    
+        # final check
+        longest_window = max(len(window), longest_window)
         return longest_window
