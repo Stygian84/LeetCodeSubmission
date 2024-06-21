3271e2ad4da46b105afff88ca3a6fbe0f2858b1e
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-21 11:28:54 +0800

    Time: 95 ms (26.90%) | Memory: 16.6 MB (86.65%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -7,12 +7,11 @@ class Solution:
         arr=deque([])
         min_score=math.inf
         for item in nums:
+            arr.append(item)
+            if len(arr)>k:
+                arr.popleft()
             if len(arr)==k:
                 diff=arr[-1]-arr[0]
                 min_score=min(min_score,diff)
-                arr.popleft()
-            arr.append(item)
-        diff=arr[-1]-arr[0]
-        min_score=min(min_score,diff)
 
         return min_score
