d2ba022b45d1dd566d561ce6eb1e87c37ec4f26d
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-24 16:30:32 +0800

    Time: 108 ms (23.02%) | Memory: 16.4 MB (90.72%) - LeetSync

---
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def alternatingSubarray(self, nums: List[int]) -> int:
+            max_length=-1
+
+
+            for i in range(len(nums)-1):
+
+                length=1
+                flag=True
+
+                for j in range(i,len(nums)-1):
+                    if flag and nums[j+1]==nums[j]+1:
+                        flag=False
+                        length+=1
+                    elif not flag and nums[j+1]==nums[j]-1:
+                        flag=True
+                        length+=1
+                    else:
+                        break
+                    max_length=max(max_length,length)
+
+            if max_length>=2:
+                return max_length
+                
+            return -1
