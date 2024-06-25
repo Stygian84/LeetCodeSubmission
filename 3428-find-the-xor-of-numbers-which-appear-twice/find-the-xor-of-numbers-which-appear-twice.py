576302e4ac01d18b22c8c8ebc4834949bc043bf0
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-25 16:03:56 +0800

    Time: 53 ms (23.41%) | Memory: 16.4 MB (64.89%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.py
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.py
@@ -1,5 +1,13 @@
 class Solution:
     def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        '''result = 0
+        for i in range(len(nums)):
+            num = abs(nums[i])
+            if nums[num - 1] < 0:
+                result ^= num
+            else:
+                nums[num - 1] = -nums[num - 1]
+        return result'''
         dc={}
         for num in nums:
             dc[num]=dc.get(num,0)+1
