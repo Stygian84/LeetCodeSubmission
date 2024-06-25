066c6ef212980f7eadf37d8c148e76c21f7ede56
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-25 11:49:17 +0800

    Time: 43 ms (14.86%) | Memory: 16.5 MB (46.50%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -9,9 +9,20 @@ class Solution:
         
         x=abs(x)
 
+        res=0
+        while x != 0:
+            digit = x % 10
+            x //= 10
 
+            if res > (max_value - digit) // 10:
+                return 0
+
+            res = res * 10 + digit
+
+        return sign * res
+        '''
         res=int(str(x)[::-1]) * sign
 
         if res<min_value or res>max_value:
             return 0
-        return res
+        return res'''
