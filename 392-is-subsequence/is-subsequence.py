bc269d6618e50f7f030a3585d54c2ec5b01bd47d
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-26 10:39:10 +0800

    Time: 36 ms (55.82%) | Memory: 16.5 MB (68.93%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,6 +1,26 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        idx=0
+        if not s:
+            return True
+        if not t:
+            return False
+        left_s=0
+        right_s=len(s)-1
+        left_t=0
+        right_t=len(t)-1
+
+        while left_t<right_t:
+            if s[left_s]==t[left_t]:
+                left_s+=1
+            if s[right_s]==t[right_t]:
+                right_s-=1
+            left_t+=1
+            right_t-=1
+        if s[left_s]==t[left_t]: #for even length of s
+            left_s+=1
+        
+        return left_s>right_s
+        '''idx=0
         if s=="":
             return True
         for letter in t:
@@ -12,4 +32,4 @@ class Solution:
         if (idx)!=len(s):
             return False
         else:
-            return True
+            return True'''
