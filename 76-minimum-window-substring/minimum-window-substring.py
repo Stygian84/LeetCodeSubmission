92db1ea3ae4a95ed114024b4971aff10c35396ff
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-26 11:58:19 +0800

    Time: 184 ms (30.08%) | Memory: 17.2 MB (90.05%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,7 +1,42 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        dc=defaultdict(int)
+        if not s or not t:
+            return ""
+        
+        target_counts = defaultdict(int)
+        for char in t:
+            target_counts[char] += 1
+        
+        required_chars = len(target_counts)
+        left, right = 0, 0
+        formed = 0
+        window_counts = defaultdict(int)
+        min_len = float('inf')
+        min_window = ""
+        
+        while right < len(s):
+            char = s[right]
+            window_counts[char] += 1
+            
+            if char in target_counts and window_counts[char] == target_counts[char]:
+                formed += 1
+            
+            while formed == required_chars and left <= right:
+                current_len = right - left + 1
+                if current_len < min_len:
+                    min_len = current_len
+                    min_window = s[left:right+1]
+                
+                window_counts[s[left]] -= 1
+                if s[left] in target_counts and window_counts[s[left]] < target_counts[s[left]]:
+                    formed -= 1
+                
+                left += 1
+            
+            right += 1
+        
+        return min_window
+        '''dc=defaultdict(int)
         for letter in t:
             dc[letter]+=1
 
@@ -24,5 +59,5 @@ class Solution:
                 i+=1
             
 
-        return res
+        return res'''
         
