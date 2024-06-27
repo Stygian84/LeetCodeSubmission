55ddcfcdfb7c503b92fc1a4f3298ce3a322b9cba
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-27 16:26:07 +0800

    Time: 61 ms (31.78%) | Memory: 16.7 MB (18.29%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -5,10 +5,7 @@ class Solution:
         res=[words[0]]
         
         for i in range(1,len(words)):
-            str1="".join(sorted(list(words[i-1])))
-            str2="".join(sorted(list(words[i])))
-
-            if str1!=str2:
+            if (sorted(list(words[i-1]))) != (sorted(list(words[i]))):
                 res.append(words[i])
 
         return res
