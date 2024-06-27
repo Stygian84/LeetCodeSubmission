96ec98fce078d8a7836915d6dd8c823aaa6cd5fb
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-06-27 17:07:04 +0800

    Time: 709 ms (23.50%) | Memory: 30.5 MB (62.85%) - LeetSync

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        count=0
+        min_value=nums[0]
+        for i in range(1,len(nums)):
+            if nums[i]-min_value>k:
+                min_value=nums[i]
+                count+=1
+        count+=1
+        return count
