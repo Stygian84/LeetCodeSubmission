12d61606b2816be24a64e94d5d5efed56312b512
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-03 14:17:42 +0800

    Time: 368 ms (64.61%) | Memory: 38.6 MB (27.88%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,22 +1,27 @@
 class MedianFinder:
 
     def __init__(self):
-        self.ls=[]
-        self.i=0
-        self.count=0
+        self.low = []  
+        self.high = [] 
 
     def addNum(self, num: int) -> None:
-        bisect.insort(self.ls, num)
+        heapq.heappush(self.low, -num)
         
-        self.count+=1
-        if self.count==2:
-            self.count=0
-            self.i+=1
+        if self.low and self.high and (-self.low[0] > self.high[0]):
+            val = -heapq.heappop(self.low)
+            heapq.heappush(self.high, val)
+        if len(self.low) > len(self.high) + 1:
+            val = -heapq.heappop(self.low)
+            heapq.heappush(self.high, val)
+        
+        if len(self.high) > len(self.low):
+            val = heapq.heappop(self.high)
+            heapq.heappush(self.low, -val)
 
     def findMedian(self) -> float:
-        if self.count==0:
-            return (self.ls[self.i]+self.ls[self.i-1])/2
-        return self.ls[self.i]
+        if len(self.low) > len(self.high):
+            return -self.low[0]
+        return (-self.low[0] + self.high[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
