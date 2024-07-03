4d8478410c2886a77520ec512bd08ddb42633082
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-03 17:58:22 +0800

    Time: 618 ms (28.08%) | Memory: 22.7 MB (76.77%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,8 +1,18 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def sqrt_sum_squares(point):
+        def distance(point):
             return sqrt(point[0]**2 + point[1]**2)
-
-        sorted_points = sorted(points, key=sqrt_sum_squares)
         
-        return sorted_points[:k]
+        max_heap = []
+        
+        for point in points:
+            dist = distance(point)
+            
+            heapq.heappush(max_heap, (-dist, point))
+            
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        
+        closest_points = [point for _, point in max_heap]
+        
+        return closest_points
