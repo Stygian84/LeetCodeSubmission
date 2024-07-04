cfc35d8cf76c82caea01907038ef54af9299f913
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-04 18:01:18 +0800

    Time: 546 ms (33.50%) | Memory: 42.1 MB (57.90%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.py
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,29 +1,9 @@
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
-        def reverse(x):
-            min_value=-2**31
-            max_value=2**31-1
-
-            sign=1
-            if x<0:
-                sign=-1
-            
-            x=abs(x)
-
-            res=0
-            while x != 0:
-                digit = x % 10
-                x //= 10
-
-                if res > (max_value - digit) // 10:
-                    return 0
-
-                res = res * 10 + digit
-
-            return sign * res
+        
         s=set()
 
         for num in nums:
             s.add(num)
-            s.add(reverse(num))
+            s.add(int(str(num)[::-1]))
         return len(s)
