16507f30fee24c06bb17d8b2de8084f7f7c2a167
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-05 13:00:35 +0800

    Time: 40 ms (65.27%) | Memory: 16.6 MB (30.16%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.py
+++ 862-find-and-replace-in-string/find-and-replace-in-string.py
@@ -4,16 +4,10 @@ class Solution:
 
         dc={}
         for a,b,c in zip(indices,sources,targets):
-            if a in dc:
-                #check which one is valid
-                source1=dc[a][0]
-                n=len(source1)
-                if s[a:a+n]!=source1:
-                    dc[a]=[b,c]
-            
-            else:
+            n=len(b)
+            if s[a:a+n]==b: #check if valid
                 dc[a]=[b,c]
-        print(dc)
+            
         skip=0
         for i in range(len(s)):
             
@@ -23,12 +17,9 @@ class Solution:
 
             if i in dc:
                 n=len(dc[i][0])
-                
-                if s[i:i+n]==dc[i][0]:
-                    res.append(dc[i][1])
-                    skip=n-1
-                else:
-                    res.append(s[i])
+                res.append(dc[i][1])
+                skip=n-1
+
             else:
                 res.append(s[i])
         
