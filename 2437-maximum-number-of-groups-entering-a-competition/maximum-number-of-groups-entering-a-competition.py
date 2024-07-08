b74cc12eb27410b1406a0aef63aa55b3286e8a40
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-08 14:49:43 +0800

    Time: 397 ms (45.89%) | Memory: 29.8 MB (44.86%) - LeetSync

--- 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.py
+++ 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.py
@@ -1,9 +1,23 @@
 class Solution:
     def maximumGroups(self, grades: List[int]) -> int:
-        total=0
+        n = len(grades)
+        
+        left, right = 1, n
+        result = 1 
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * (mid + 1) // 2 <= n:
+                result = mid  
+                left = mid + 1 
+            else:
+                right = mid - 1 
+        
+        return result
+        '''total=0
         n = len(grades)
         for i in range(1,n+1):
             total+=i
             if total>n:
                 return i-1
-        return 1
+        return 1'''
