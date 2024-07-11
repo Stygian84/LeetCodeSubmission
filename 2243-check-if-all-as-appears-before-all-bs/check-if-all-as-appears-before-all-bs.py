059ba09c95a6aae55e76844e0d8172ea04b4610e
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-11 13:36:13 +0800

    Time: 41 ms (15.14%) | Memory: 16.4 MB (61.21%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
@@ -1,17 +1,11 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        result=None
-        if s[0]=='b' and s[-1]=='a':
-            return False
-        for item in s:
-            if item=="a" and result==None:
-                result=True
-            elif result and item=="b":
-                result=False
-            elif result==False and item=="a":
+        flag=False
+        for i in s:
+            if i=="b":
+                flag=True
+            elif flag:
                 return False
-            
-
         return True
 
             
