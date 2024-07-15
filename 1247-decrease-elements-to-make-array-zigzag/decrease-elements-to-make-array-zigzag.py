3ea0456919d82335137231cccf93515acd9bb8cb
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-15 17:46:20 +0800

    Time: 78 ms (5.17%) | Memory: 16.5 MB (97.41%) - LeetSync

--- 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.py
+++ 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.py
@@ -4,35 +4,31 @@ class Solution:
             return 0
         even_count=0
         arr=nums[:]
-        for i in range(len(nums)):
+        for i in range(0,len(nums),2):
             if i==0:
                 while nums[i]>=nums[i+1]:
                     nums[i]-=1
                     even_count+=1
                 continue
-
-            if i%2==0:
-                
-                if i==len(nums)-1:
-                    while nums[i]>=nums[i-1]:
-                        nums[i]-=1
-                        even_count+=1
-                    continue
-                while nums[i]>=nums[i-1] or nums[i]>=nums[i+1]:
+            if i==len(nums)-1:
+                while nums[i]>=nums[i-1]:
                     nums[i]-=1
                     even_count+=1
+                continue
+            while nums[i]>=nums[i-1] or nums[i]>=nums[i+1]:
+                nums[i]-=1
+                even_count+=1
                     
         odd_count=0
-        for i in range(len(arr)):
-            if i%2==1:
-                if i==len(arr)-1:
-                    while arr[i]>=arr[i-1]:
-                        arr[i]-=1
-                        odd_count+=1
-                    continue
-                while arr[i]>=arr[i-1] or arr[i]>=arr[i+1]:
+        for i in range(1,len(arr),2):
+            if i==len(arr)-1:
+                while arr[i]>=arr[i-1]:
                     arr[i]-=1
                     odd_count+=1
+                continue
+            while arr[i]>=arr[i-1] or arr[i]>=arr[i+1]:
+                arr[i]-=1
+                odd_count+=1
                     
         if odd_count<even_count:
             return odd_count
