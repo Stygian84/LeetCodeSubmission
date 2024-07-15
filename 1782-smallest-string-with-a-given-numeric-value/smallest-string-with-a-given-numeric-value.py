c728903ad1a63dbe05840d571b66acd8a8315a96
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-15 16:00:53 +0800

    Time: 219 ms (70.92%) | Memory: 18.3 MB (25.51%) - LeetSync

--- 1782-smallest-string-with-a-given-numeric-value/smallest-string-with-a-given-numeric-value.py
+++ 1782-smallest-string-with-a-given-numeric-value/smallest-string-with-a-given-numeric-value.py
@@ -1,13 +1,13 @@
 class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
-        res=""
+        res=[]
         total=0
         count=0
         # a = 1,z=26
         # 1 1 x 26 26 = 73-54 = 19
         # 72 71 52 78
         while n-count>1 and 26*(n-count-1)>=k-total:
-            res+="a"
+            res.append("a")
             count+=1
             total+=1
         
@@ -17,9 +17,9 @@ class Solution:
         count_z=remaining//26
         leftover = remaining - 26 * count_z
         if leftover > 0:
-            res+=(chr(leftover + ord('a') - 1))
+            res.append(chr(leftover + ord('a') - 1))
             
-        res+="z"*count_z
+        res.append("z"*count_z)
 
-        return res
+        return "".join(res)
 
