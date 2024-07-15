07a26bff0d20fef15d9ebff4dad2bb3f3c7dd644
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-15 17:48:55 +0800

    Time: 68 ms (5.17%) | Memory: 16.7 MB (11.21%) - LeetSync

--- 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.py
+++ 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.py
@@ -6,14 +6,16 @@ class Solution:
         arr=nums[:]
         for i in range(0,len(nums),2):
             if i==0:
-                while nums[i]>=nums[i+1]:
-                    nums[i]-=1
-                    even_count+=1
+                if nums[i]>=nums[i+1]:
+                    diff=nums[i]-nums[i+1]+1
+                    nums[i]-=diff
+                    even_count+=diff
                 continue
             if i==len(nums)-1:
-                while nums[i]>=nums[i-1]:
-                    nums[i]-=1
-                    even_count+=1
+                if nums[i]>=nums[i-1]:
+                    diff=nums[i]-nums[i-1]+1
+                    nums[i]-=diff
+                    even_count+=diff
                 continue
             while nums[i]>=nums[i-1] or nums[i]>=nums[i+1]:
                 nums[i]-=1
@@ -22,9 +24,10 @@ class Solution:
         odd_count=0
         for i in range(1,len(arr),2):
             if i==len(arr)-1:
-                while arr[i]>=arr[i-1]:
-                    arr[i]-=1
-                    odd_count+=1
+                if arr[i]>=arr[i-1]:
+                    diff = arr[i]-arr[i-1]+1
+                    arr[i]-=diff
+                    odd_count+=diff
                 continue
             while arr[i]>=arr[i-1] or arr[i]>=arr[i+1]:
                 arr[i]-=1
