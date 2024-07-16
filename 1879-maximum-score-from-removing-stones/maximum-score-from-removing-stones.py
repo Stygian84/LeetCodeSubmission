40920e897c078a8b8e3c98b49c2c2940c2ce8543
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-16 11:14:51 +0800

    Time: 40 ms (73.75%) | Memory: 16.6 MB (8.33%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.py
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.py
@@ -1,6 +1,13 @@
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        ls=[-a,-b,-c] #maxheap
+        ls=[a,b,c]
+        ls.sort()
+
+        total = ls[0]+ls[1]
+        if total > ls[-1]:
+            return ls[-1] + (total-ls[-1]) //2
+        return total
+        '''ls=[-a,-b,-c] #maxheap
         heapq.heapify(ls)
         count=0
         while len(ls)>1:
@@ -13,5 +20,5 @@ class Solution:
             if maximum!=0:
                 heapq.heappush(ls,-maximum)
                 
-        return count
+        return count'''
 
