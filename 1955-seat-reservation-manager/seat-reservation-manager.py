21a7826fdd549b1bc89cbddf11c40fb1b249a510
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-16 11:25:35 +0800

    Time: 314 ms (96.50%) | Memory: 42.5 MB (98.36%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.py
+++ 1955-seat-reservation-manager/seat-reservation-manager.py
@@ -2,13 +2,19 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.ls = []
-        [heapq.heappush(self.ls, item) for item in range(1,n+1)]
+        self.pointer = 1
 
     def reserve(self) -> int:
-        return heapq.heappop(self.ls)
+        if len(self.ls)>0:
+            res=self.ls[0]
+            heapq.heappop(self.ls)
+        else:
+            res=self.pointer
+            self.pointer+=1
+        return res
 
     def unreserve(self, seatNumber: int) -> None:
-        return heapq.heappush(self.ls,seatNumber)
+        heapq.heappush(self.ls,seatNumber)
 
 
 # Your SeatManager object will be instantiated and called as such:
