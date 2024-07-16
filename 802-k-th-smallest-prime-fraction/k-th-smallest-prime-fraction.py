5c731ee4cdf648b09c0fc17c26d67f0d7c6ed925
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-16 11:39:11 +0800

    Time: 580 ms (59.16%) | Memory: 16.9 MB (52.47%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,6 +1,18 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        dc={}
+
+        n = len(arr)
+        min_heap = []
+        
+        for j in range(1, n):
+            heapq.heappush(min_heap, (arr[0] / arr[j], 0, j))
+        for _ in range(k):
+            frac, i, j = heapq.heappop(min_heap)
+            if i + 1 < j:
+                heapq.heappush(min_heap, (arr[i+1] / arr[j], i+1, j))
+        
+        return [arr[i], arr[j]]
+        '''dc={}
         n=len(arr)
         ls=[]
         for i in range(n-1):
@@ -13,4 +25,4 @@ class Solution:
         for i in range(k):
             value=heapq.heappop(ls)
         
-        return dc[value]
+        return dc[value]'''
