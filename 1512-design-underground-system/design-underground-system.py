dbddb6f08e13edc17695b2fb8340cca4545bd260
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-17 13:49:27 +0800

    Time: 218 ms (12.80%) | Memory: 28.1 MB (6.16%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,29 +1,33 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.check_ins = {}  # {id: (start_station, start_time)}
-        self.avg_times = {}  # {(start_station, end_station): (total_time, count)}
+        self.dc = {}  # {id: (start_station, start_time)}
+        self.avg = {}  # {(start_station, end_station): (total_time, count)}
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_ins[id] = (stationName, t)
+        self.dc[id]=(stationName,t)
         
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start_station, start_time = self.check_ins[id]
-        journey_time = t - start_time
-        route = (start_station, stationName)
+        a=self.dc[id][0]
+        b=stationName
+        c=t
+        d=self.dc[id][1]
 
-        if route in self.avg_times:
-            total_time, count = self.avg_times[route]
-            self.avg_times[route] = (total_time + journey_time, count + 1)
+        #self.dc[id]=[a+b,c-d]
+        
+        #add to self.avg
+        if (a,b) in self.avg:
+            prev_total = self.avg[(a,b)][0]
+            prev_count = self.avg[(a,b)][1]
+
+            self.avg[(a,b)] = [prev_total + (c-d), prev_count+1] 
         else:
-            self.avg_times[route] = (journey_time, 1)
+            self.avg[(a,b)] = [c-d,1]
         
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        route = (startStation, endStation)
-        total_time, count = self.avg_times[route]
-        return total_time / count
+        return self.avg[(startStation,endStation)][0] / self.avg[(startStation,endStation)][1]
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
