88a6d9813ba3193ceb38c08759f166ae99082706
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-18 17:23:13 +0800

    Time: 41 ms (29.65%) | Memory: 16.5 MB (52.57%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.py
@@ -0,0 +1,51 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+
+        freq = Counter(s)
+
+        max_heap = []
+        for key,value in freq.items():
+            heapq.heappush(max_heap, (-value,key) )
+
+        res = []
+        prev_count,prev_char = 0 , ""
+        while max_heap:
+            count, char = heapq.heappop(max_heap)
+            res.append(char)
+            
+            if prev_count < 0:
+                heapq.heappush(max_heap, (prev_count, prev_char))
+            
+            prev_count, prev_char = count + 1, char  
+        
+        res_str = ''.join(res)
+        if len(res_str)!=len(s):
+            return ""
+        for i in range(1, len(res_str)):
+            if res_str[i] == res_str[i - 1]:
+                return ""
+        return res_str
+        '''dc={}
+        for letter in s:
+            dc[letter]=dc.get(letter,0)+1
+        res=""
+        prev_values = None
+        while True:
+            current_values = list(dc.values())
+            print(current_values)
+            if prev_values is not None and current_values == prev_values:
+                if any(value != 0 for value in current_values):
+                    return ""
+                break
+            prev_values = current_values
+            for key, values in sorted(dc.items(), key=lambda x: x[1], reverse=True):
+                if res=="":
+                    res+=key
+                    dc[key]-=1
+                    continue
+                if values>0 and res[-1]!=key:
+                    res+=key
+                    dc[key]-=1
+            print(res)
+        return res
+        '''
