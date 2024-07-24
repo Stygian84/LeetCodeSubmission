ff3b6622154b98693cc0bd4830e3238070eba56e
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-24 15:05:13 +0800

    Time: 37 ms (41.54%) | Memory: 16.4 MB (91.28%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        ls = list(str(num))
+        #find maximum, swap iwth num[0] if maximum>num[0]
+        #else,
+        maximum=[]
+        for i in range(len(ls)-1):
+            if ls[i]<=ls[i+1]:
+                maximum.append((ls[i+1],i+1))
+        
+        maximum.sort(key=lambda x: (x[0], x[1]),reverse=True)
+        
+        for a,b in maximum:
+            i=0
+            while i<len(ls):
+                if a>ls[i] and b>i:
+                    ls[i],ls[b]=ls[b],ls[i]
+                    return int("".join(ls))
+                i+=1
+        return num
