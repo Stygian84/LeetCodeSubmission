5deab6bc42dd0f9696506513232223e15f738ee2
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-25 17:26:34 +0800

    Time: 763 ms (94.87%) | Memory: 30.9 MB (90.34%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
@@ -3,35 +3,21 @@ class Solution:
 
         # check increasing -> decreasing -> increasing
         # at increasing find max, at decreasing find min
+        # max and min are always at turning point
         total = 0
 
         increasing = True
         for i in range(1,len(nums)):
             if nums[i]<nums[i-1]:
-                if increasing:
+                if increasing: #if increasing, add maximum
                     total+=nums[i-1]
                     increasing = False
             else:
-                if not increasing:
-                    increasing = True
+                if not increasing: #if decreasing, decrease minimum
                     total-=nums[i-1]
+                    increasing = True
         
-        if increasing:
+        if increasing: #add last element if end at increasing
             total+=nums[-1]
 
-
         return total
-        '''odd_sum=[nums[1]]
-        even_sum=[nums[0]]
-        
-        n = len(nums)
-        res = 0
-        for i in range(2,n):
-            if i%2==0:
-                even_sum.append(even_sum[-1]+nums[i])
-            else:
-                odd_sum.append(odd_sum[-1]+nums[i])
-            res = max(res,nums[i],even_sum[-1]-odd_sum[-1])
-        print(even_sum,odd_sum)
-
-        return res'''
