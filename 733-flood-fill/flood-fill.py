083ccca290909b24601e7e9bb5d92188128d6d27
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-26 15:37:59 +0800

    Time: 73 ms (18.10%) | Memory: 16.5 MB (86.16%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,17 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         original = image[sr][sc]
+        if original == color:
+            return image
         def dfs(grid,x,y,visited,color):
             if (x,y) in visited:
                 return
             visited.add((x,y))
+            grid[x][y]=color
 
             directions = [(0,1),(0,-1),(1,0),(-1,0)]
             for dx,dy in directions:
                 nx,ny = x+dx,y+dy
 
                 if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny]==original:
-                    grid[nx][ny]=color
                     dfs(grid,nx,ny,visited,color)
 
 
@@ -21,5 +23,4 @@ class Solution:
         visited = set()
 
         dfs(image,sr,sc,visited,color)
-        image[sr][sc]=color
         return image
