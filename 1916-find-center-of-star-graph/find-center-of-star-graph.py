1851054fb303cf70718a81419eb0b9ed769cf5b7
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-29 13:25:12 +0800

    Time: 649 ms (66.93%) | Memory: 51.9 MB (95.22%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,9 +1,13 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        first_digit=edges[0][0]
+        if edges[0][1] == edges[1][0] or edges[0][1] == edges[1][1]:
+            return edges[0][1]
+        else:
+            return edges[0][0]
+        '''first_digit=edges[0][0]
         second_digit=edges[0][1]
         if first_digit in edges[1]:
             return first_digit
         else:
-            return second_digit
+            return second_digit'''
         
