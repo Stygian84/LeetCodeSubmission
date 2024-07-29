fba597f0c0fee6679e3a69af4947f856424f3a92
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-07-29 11:50:43 +0800

    Time: 69 ms (42.87%) | Memory: 18.1 MB (7.34%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        n = len(nums)
+        res = []
+        new = []
+        for i in range(n):
+            new.append( (nums[i], i) )
+        
+        new.sort()
+        l,r = 0 , n-1
+        while l<r:
+            total = new[l][0]+new[r][0]
+            if total==target:
+                return [new[l][1],new[r][1]]
+            elif total<target:
+                l+=1
+                while l<r and new[l][0]==new[l-1][0]:
+                    l+=1
+            else:
+                r-=1
+                while l<r and new[r][0]==new[r+1][0]:
+                    r-=1
 
-        ls = []
+        '''ls = []
         for idx in range(0, len(nums)):
             new_num = target - nums[idx]
             if new_num in nums[idx + 1 :]:
@@ -9,4 +30,4 @@ class Solution:
                 ls.append(nums[idx + 1 :].index(new_num) + idx + 1)
                 return ls
             else:
-                continue
+                continue'''
