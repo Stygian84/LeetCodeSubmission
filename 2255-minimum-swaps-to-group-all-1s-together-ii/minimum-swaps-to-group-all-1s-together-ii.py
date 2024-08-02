2af052039d9dd7327a01535544280f93e025db5c
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-02 10:04:44 +0800

    Time: 844 ms (5.85%) | Memory: 21.9 MB (5.18%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        n=nums.count(1)
+        if n == 0:
+            return 0
+        nums = nums*3
+        res = math.inf
+
+
+        k = 0
+        count = 0
+        window = deque()
+        for i in range(len(nums)):
+            if nums[i]==0:
+                count+=1
+            window.append(nums[i])
+            k+=1
+            if k==n:
+                res = min(res,count)
+                if window.popleft() == 0:
+                    count-=1
+                k-=1
+
+        return res
