41f5d403d6d8f38b91fab19ac23a26f03de869e9
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-02 11:56:02 +0800

    Time: 103 ms (24.68%) | Memory: 17.8 MB (16.92%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.py
+++ 944-smallest-range-i/smallest-range-i.py
@@ -1,6 +1,7 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        a,b=min(nums),max(nums)
+        nums.sort()
+        a,b=nums[0],nums[-1]
         if a+k>=b-k:
             return 0
         return (b-k)-(a+k)
