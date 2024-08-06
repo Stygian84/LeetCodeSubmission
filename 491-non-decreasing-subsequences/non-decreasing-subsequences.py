a2500d01c25567e3eb25e65a99370481293ba372
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-06 12:15:08 +0800

    Time: 148 ms (49.62%) | Memory: 29.6 MB (7.21%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.py
@@ -1,13 +1,15 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         res = []
-
+        seen = set()
         n = len(nums)
 
         def backtrack(start,path):
             nonlocal res
             if len(path)>=2:
-                if path[:] not in res:
+                x=tuple(path)
+                if x not in seen:
+                    seen.add(x)
                     res.append(path[:])
             
             for i in range(start,n):
