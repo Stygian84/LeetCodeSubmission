fe009466cf9b766de9e35ae064361df40a837535
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-07 15:37:32 +0800

    Time: 49 ms (58.40%) | Memory: 16.6 MB (60.61%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        res = ""
+
+        def backtrack(path):
+            nonlocal k,res
+            if len(path)==n:
+                print(path,k)
+                k-=1
+                if k==0:
+                    res="".join(path[:])
+                return
+            if res:
+                return
+
+            for item in "abc":
+                if not path:
+                    path.append(item)
+                    backtrack(path)
+                    path.pop()
+
+                if (path and path[-1]!=item):
+                    path.append(item)
+                    backtrack(path)
+                    path.pop()
+
+
+        backtrack([])
+        print(res)
+        return res
