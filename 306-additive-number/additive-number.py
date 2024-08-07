b881e34a65e284357f2d6deb509c6cba4f9a52b2
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-07 11:46:23 +0800

    Time: 39 ms (38.59%) | Memory: 16.6 MB (28.81%) - LeetSync

---
+++ 306-additive-number/additive-number.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        
+        res = []
+        n = len(num)
+        def backtrack(start,path):
+            nonlocal res
+            if start==n and len(path)>2:
+                res = path[:]
+                return
+            if res:
+                return
+            
+            for i in range(start,n+1):
+                str_num = num[start:i+1]
+                
+                if not str_num:
+                    continue
+                if str_num[0]=="0" and len(str_num)>1:
+                    break
+                number = int(str_num)
+
+                if len(path)>=2 and path[-1]+path[-2]==number:
+                    path.append(number)
+                    backtrack(i+1,path)
+                    path.pop()
+                elif len(path)<2:
+                    path.append(number) 
+                    backtrack(i+1,path)
+                    path.pop()
+
+        backtrack(0,[])
+        if res:
+            return True
+        return False
