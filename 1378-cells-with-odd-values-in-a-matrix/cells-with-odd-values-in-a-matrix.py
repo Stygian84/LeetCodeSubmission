c944f4618711d6e92ff034544277de355fe1f1f8
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-09 13:40:24 +0800

    Time: 84 ms (8.15%) | Memory: 16.5 MB (72.10%) - LeetSync

--- 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.py
+++ 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.py
@@ -4,14 +4,9 @@ class Solution:
 
         matrix = [[0]*n for _ in range(m)]
         
-        x = len(indices)
-        last = False
         count = 0
 
-        for idx in range(x):
-            r,c = indices[idx]
-            if idx == x-1:
-                last = True
+        for r,c in indices:
 
             for i in range(m):    
                 for j in range(n):
@@ -19,9 +14,11 @@ class Solution:
                         matrix[i][j]+=1
                     if j==c:
                         matrix[i][j]+=1
-                    if last:
-                        if matrix[i][j]%2==1:
-                            count+=1
+                        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]%2==1:
+                    count+=1
 
         return count
                 
