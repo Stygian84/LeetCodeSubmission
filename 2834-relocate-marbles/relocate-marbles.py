24a28255afff2140cc065fc632f7390138683b5b
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-13 15:03:21 +0800

    Time: 677 ms (20.42%) | Memory: 39.7 MB (14.14%) - LeetSync

---
+++ 2834-relocate-marbles/relocate-marbles.py
@@ -0,0 +1,22 @@
+class Solution:
+    def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
+        
+        dc = Counter(nums)
+        
+        i = 0
+        n = len(moveFrom)
+        while i < n:
+            if moveTo[i]!=moveFrom[i]:
+                dc[moveTo[i]]=dc.get(moveFrom[i],0)+1
+                dc[moveFrom[i]]=0
+            i+=1
+
+        res = []
+        for k,v in dc.items():
+            if v>0:
+                res.append(k)
+        
+        res.sort()
+        return res
+        
+
