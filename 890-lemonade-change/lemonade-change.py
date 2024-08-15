18e5296e7bbcb48f07d05495058c59709fe15319
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-15 11:56:51 +0800

    Time: 617 ms (62.64%) | Memory: 21.2 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,6 +1,33 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        dc={}
+        change = defaultdict(int)
+
+        for money in bills:
+            if money == 5:
+                change[5]+=1
+
+            elif money == 10:
+                if change[5]>0:
+                    change[5]-=1
+                    change[10]+=1
+                else:
+                    return False
+
+            else:
+                #return 15
+                if change[10]>=1 and change[5]>=1:
+                    change[10]-=1
+                    change[5]-=1
+
+                elif change[5]>=3:
+                    change[5]-=3
+
+                else:
+                    return False
+
+        return True
+
+        '''dc={}
         for item in bills:
             if item ==5:
                 dc[item]=dc.get(item,0)+1
@@ -27,5 +54,5 @@ class Solution:
                     continue
                 else:
                     return False
-        return True
+        return True'''
         
