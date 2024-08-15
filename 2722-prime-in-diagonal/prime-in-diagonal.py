472c397e0dbab1589a3cbb3014ddbc8a4fc575c8
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-15 15:15:04 +0800

    Time: 585 ms (81.03%) | Memory: 28.6 MB (58.93%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.py
+++ 2722-prime-in-diagonal/prime-in-diagonal.py
@@ -5,7 +5,7 @@ class Solution:
             if x<=1:
                 return False
             if x>1:
-                for i in range(2,x):
+                for i in range(2,int(x**0.5)+1):
                     if x%i==0:
                         return False
             return True
