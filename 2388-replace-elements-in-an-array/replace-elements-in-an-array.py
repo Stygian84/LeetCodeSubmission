63deddae0cc7c3609eb4f13e16d3a2469c15b610
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-16 14:04:24 +0800

    Time: 1139 ms (9.14%) | Memory: 73.1 MB (20.30%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.py
@@ -0,0 +1,39 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        idx = defaultdict(list) #value:idx
+
+        for i in range(len(nums)):
+            idx[nums[i]].append(i)
+        
+        for a,b in operations:
+            idx[b] = idx[a]
+            del idx[a]
+
+        res = [0]*len(nums)
+
+        for k,v in idx.items():
+            for i in v:
+                res[i]=k
+        
+        return res
+
+
+
+        '''dc = {}
+        n = len(nums)
+
+        for i in range(n):
+            dc[nums[i]] = i
+        print(dc)        
+        dc2 = {}
+        for old,new in operations:
+            if old in dc:
+                dc2[new]=dc[old]
+            else:
+                dc2[new]=dc2[old]
+        res = nums[:]
+
+        for k,v in dc2.items():
+            res[v]=k
+        
+        return res'''
