be23133e825fc9b5804d2f5a62a390bf458254cc
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-19 15:44:16 +0800

    Time: 137 ms (44.72%) | Memory: 56.9 MB (74.06%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,6 +3,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    if (x<0){
+        return false
+    }
+    let a = x.toString()
+    
+    let i=0
+    let j=a.length-1
+
+    while (i<j){
+        if (a[i]!==a[j]){
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+    /*
     a=x.toString()
     return a===a.split("").reverse().join("")
+    */
 };
