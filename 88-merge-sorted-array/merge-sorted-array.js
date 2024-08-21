ed4c1785d42bbbbfd142b0cd5278f80a9957e40a
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-21 17:31:45 +0800

    Time: 32 ms (99.90%) | Memory: 49 MB (48.57%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let j =0
+    for (let i=m; i<n+m ; i++) {
+        nums1[i]=nums2[j]
+        j++
+    }
+    nums1.sort((a,b)=>a-b)
+
+};
