76013430ea732ddfce51fe99fd0acf0ca5b1ebb5
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-08-22 16:32:32 +0800

    Time: 56 ms (33.99%) | Memory: 48.2 MB (96.77%) - LeetSync

---
+++ 66-plus-one/plus-one.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let n = digits.length
+    let i = n-1
+    let val = 1
+    let flag = true
+    while (flag){
+        let temp = digits[i]+val
+        digits[i] = temp%10
+        if (temp>=10){
+            val = Math.floor(temp/10)
+            i--
+        }
+        else{
+            break
+        }
+    }
+    if (i<0){
+        digits.unshift(val)
+    }
+    return digits
+};
