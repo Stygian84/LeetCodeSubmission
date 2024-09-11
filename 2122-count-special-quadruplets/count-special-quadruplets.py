dbb0256ece4353ed8fbc6a303c8721c787f211e6
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-09-11 11:21:54 +0800

    Time: 597 ms (34.03%) | Memory: 16.4 MB (91.81%) - LeetSync

---
+++ 2122-count-special-quadruplets/count-special-quadruplets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n-3):
+            for j in range(i+1,n-2):
+                for k in range(j+1,n-1):
+                    for l in range(k+1,n):
+                        if nums[i]+nums[j]+nums[k]==nums[l]:
+                            count+=1
+        return count
