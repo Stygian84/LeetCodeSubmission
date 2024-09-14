ca85621955a98ee95a97a84216f806dabd238226
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-09-14 13:06:19 +0800

    Time: 702 ms (6.55%) | Memory: 31.2 MB (42.18%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -1,9 +1,6 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        freq = Counter(nums)
         maximum = max(nums)
-        if freq[maximum]==1:
-            return 1
         
         res = 0
         before = None
