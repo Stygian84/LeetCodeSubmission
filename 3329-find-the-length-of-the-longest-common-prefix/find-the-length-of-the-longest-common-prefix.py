7f312620cdadaf16a62ffe27eb01de7a1ad051ab
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-09-24 10:38:29 +0800

    Time: 1023 ms (27.12%) | Memory: 30.3 MB (47.08%) - LeetSync

---
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        
+        s = set()
+
+        for num in arr1:
+            str_num = str(num)
+            for i in range(len(str_num)):
+                s.add(str_num[:i+1])
+        
+        print(s)
+        res = 0
+        for num in arr2:
+            str_num = str(num)
+            for i in range(len(str_num)):
+                if str_num[:i+1] in s:
+                    res = max(res,i+1)
+        return res
