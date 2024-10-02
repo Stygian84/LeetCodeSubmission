7dc0bad7ddac0be890b8317b3dd992498fa3dca2
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-02 11:54:24 +0800

    Time: 757 ms (12.59%) | Memory: 24.2 MB (67.48%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,6 +1,22 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        queue = deque([])
+        freq = defaultdict(int)
+        left = 0
+        maximum = 0
+
+        for right in range(len(fruits)):
+            freq[fruits[right]] += 1
+
+            while len(freq) > 2:
+                freq[fruits[left]] -= 1
+                if freq[fruits[left]] == 0:
+                    del freq[fruits[left]]
+                left += 1
+
+            maximum = max(maximum, right - left + 1)
+
+        return maximum
+        '''queue = deque([])
         freq = defaultdict(int)
         maximum = 0
         for fruit in fruits:
@@ -14,5 +30,5 @@ class Solution:
                     del freq[removed]
             
             maximum = max(maximum,len(queue))
-            
-        return maximum
+
+        return maximum'''
