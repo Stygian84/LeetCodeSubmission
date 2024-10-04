b02783eb342122d0ef6fee2f74418de0185ea3ad
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-04 15:13:53 +0800

    Time: 662 ms (89.55%) | Memory: 24.6 MB (83.08%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.py
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.py
@@ -2,13 +2,12 @@ class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
         ls = []
         for c,r in zip(capacity,rocks):
-            ls.append((c,r))
+            ls.append(c-r)
         
-        ls.sort(key = lambda x:x[0]-x[1])
+        ls.sort()
         
         res = 0
-        for c,r in ls:
-            diff = c-r
+        for diff in ls:
             if diff>0:
                 additionalRocks-=diff
             if additionalRocks<0:
