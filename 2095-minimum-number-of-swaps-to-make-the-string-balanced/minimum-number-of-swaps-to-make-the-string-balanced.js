1d5a3e91f85a0a6b89600fe7679d1ffffd813214
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-08 11:20:21 +0800

    Time: 638 ms (5.41%) | Memory: 69.6 MB (36.04%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minSwaps = function(s) {
+    let balance = 0
+    let swap = 0
+
+    for (let i = 0 ; i<s.length;i++){
+        if (s[i]=="["){
+            balance += 1
+        }
+        else if (s[i]=="]"){
+            balance -= 1
+        }
+        if (balance<0){
+            console.log(s[i],balance,swap)
+            swap += 1
+            balance+=2
+        }
+    }
+    return swap
+    
+};
