1012e4a38e68b1941a7efc82df58e28562416d28
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-09 14:33:05 +0800

    Time: 74 ms (64.75%) | Memory: 54.1 MB (57.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -10,13 +10,9 @@ var moveZeroes = function(nums) {
     let j = 0
     for (let i=0;i<n;i++){
         if (nums[i]!=0){
-            nums[j]=nums[i]
+            [nums[i],nums[j]]=[nums[j],nums[i]]
             j+=1
         }
-    }
-    while (j<n){
-        nums[j]=0
-        j+=1
     }
     return nums
 };
