7daea9f493f6b88418e17c36830a63caef8d445b
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-09 14:17:34 +0800

    Time: 54 ms (42.88%) | Memory: 49 MB (65.80%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let res = []
+    function traverse(node){
+        if (node){
+            traverse(node.left)
+            traverse(node.right)
+            res.push(node.val)
+        }
+    }
+    traverse(root)
+    return res
+    
+};
