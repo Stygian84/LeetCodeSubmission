9028c5b3e51103cee1dfde3aeb91ee35fa6ad0cb
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-17 13:56:25 +0800

    Time: 657 ms (8.19%) | Memory: 22.7 MB (5.94%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -3,19 +3,16 @@ class Solution:
         #try convert 0 to -1 and add, if equal to 0, equal
         n = len(nums)
         prefix = [0]*(n)
+        
         longest = 0 #furthest 0 or length between one 0  and one -1
         
-        first_zero = -1
-        first_one = -1
-        last_zero = -1 #always equal to first longest
-        last_one = -1
         for i in range(n):
             prefix[i]=prefix[i-1]
             if nums[i]==1:
                 prefix[i]+=1
             else:
                 prefix[i]-=1     
-                
+
             if prefix[i]==0:
                 longest = i+1
             
