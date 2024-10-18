c0e47ebcda81ff983e1b5f43b87875fa5f683d1b
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-18 16:43:15 +0800

    Time: 30 ms (100.00%) | Memory: 21.5 MB (98.62%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -3,18 +3,18 @@ class Solution:
         n = len(values)
 
         res = 0
-        best_value = (values[0],0)
+        best_value = values[0]
         # find maximum values[i]+i and values[j]-j where i<j
         
         for i in range(1,n):
             pointer = values[i]+i
             curr_value = values[i]-i
 
-            if curr_value + best_value[0]>res:
-                res = curr_value + best_value[0]
+            if curr_value + best_value>res:
+                res = curr_value + best_value
             
-            if pointer>best_value[0]:
-                best_value = (pointer,i)
+            if pointer>best_value:
+                best_value = pointer
             
             #print(values[i]+i , values[i]-i)
         
