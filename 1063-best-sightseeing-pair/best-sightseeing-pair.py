78903579747bf4cb4896f618a0a61a33320ff284
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-10-18 16:42:12 +0800

    Time: 27 ms (100.00%) | Memory: 21.5 MB (98.62%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         n = len(values)
-        dp = [0]*n
-        dp[0] = (values[0],0)
 
         res = 0
         best_value = (values[0],0)
         # find maximum values[i]+i and values[j]-j where i<j
+        
         for i in range(1,n):
             pointer = values[i]+i
             curr_value = values[i]-i
