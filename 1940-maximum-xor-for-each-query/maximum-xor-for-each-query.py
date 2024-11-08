09c781e9ff7066c17d62a127facebe6f30c2c449
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-11-08 11:13:25 +0800

    Time: 59 ms (53.57%) | Memory: 32.6 MB (79.65%) - LeetSync

---
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        # find non negative k < 2**maximumBit so that xor all queries + xor k is maximised
+        total = 0
+        for num in nums:
+            total ^= num
+
+
+        res = []
+
+        for num in nums[::-1]:
+            max_k = (1 << maximumBit) - 1
+
+            res.append(total ^ max_k)
+
+            total^=num 
+
+        return res
