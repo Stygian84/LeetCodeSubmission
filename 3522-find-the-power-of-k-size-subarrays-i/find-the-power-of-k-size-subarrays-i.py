343ce5cc7814e51c46e72d0fbdfa2d567f80ab2d
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-11-16 13:47:37 +0800

    Time: 204 ms (5.85%) | Memory: 17.3 MB (5.94%) - LeetSync

---
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -0,0 +1,33 @@
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        if k == 1:
+            return nums
+
+        windows = deque([])
+        n = len(nums)
+
+        res = []
+        
+        for i in range(n):
+            #check
+            if len(windows)<k:
+                windows.append(nums[i])
+            else:            
+                print(windows)
+                for j in range(1,len(windows)):
+                    if windows[j]-windows[j-1]!=1:
+                        res.append(-1)
+                        break
+                else:
+                    res.append(windows[-1])
+                windows.append(nums[i])
+                windows.popleft()
+
+        for j in range(1,len(windows)):
+            if windows[j]-windows[j-1]!=1:
+                res.append(-1)
+                break
+        else:
+            res.append(windows[-1])
+        return res
+            
