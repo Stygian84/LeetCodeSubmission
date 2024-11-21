c4df0f23277a34c49139bda35f282ee966784670
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-11-21 14:50:54 +0800

    Time: 119 ms (43.91%) | Memory: 28.2 MB (79.83%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -10,20 +10,15 @@ class Solution:
         total = 0
         print(target)
         for i in range(n):
+            window.append(nums[i])
+            total += nums[i]
+    
             while window and total>target:
                 total -= window.popleft()
             if total == target:
                 res = min(res,n-len(window))
             
-            window.append(nums[i])
-            total += nums[i]
-    
-        
-        while window and total>target:
-            total -= window.popleft()
-        if total == target:
-            res = min(res,n-len(window))
-            
+
         if res == math.inf:
             return -1
         return res
