ce236026afdf27345496bc498169007b5c9580e9
Nicholas <91217091+Stygian84@users.noreply.github.com>
2024-11-21 14:49:42 +0800

    Time: 115 ms (46.85%) | Memory: 28.2 MB (79.83%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+
+        #find longest subsequence whose total is sum(nums)-x
+        res = math.inf
+
+        window = deque([])
+        target = sum(nums)-x
+        total = 0
+        print(target)
+        for i in range(n):
+            while window and total>target:
+                total -= window.popleft()
+            if total == target:
+                res = min(res,n-len(window))
+            
+            window.append(nums[i])
+            total += nums[i]
+    
+        
+        while window and total>target:
+            total -= window.popleft()
+        if total == target:
+            res = min(res,n-len(window))
+            
+        if res == math.inf:
+            return -1
+        return res
