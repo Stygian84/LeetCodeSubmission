73e09a907214b752116d8c257d54a48b5de08975
Nicholas <91217091+Stygian84@users.noreply.github.com>
2025-01-20 14:27:14 +0800

    Time: 824 ms (38.02%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 select name
-from customer
-where referee_id != 2 or referee_id is null;
+from customer c1
+where referee_id <> 2 or referee_id is null
