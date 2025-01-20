d265696f347399fff98e392a41d71382d3d0d9b9
Nicholas <91217091+Stygian84@users.noreply.github.com>
2025-01-20 14:07:24 +0800

    Time: 1612 ms (21.77%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,8 +1,9 @@
-/* Write your PL/SQL query statement below */
-DELETE FROM person p1
-WHERE EXISTS (
-    SELECT 1
+# Write your MySQL query statement below
+DELETE p1
+FROM person p1
+LEFT JOIN (
+    SELECT MIN(p2.id) AS id
     FROM person p2
-    WHERE p1.email = p2.email
-    AND p1.id > p2.id
-);
+    GROUP BY p2.email
+) AS valid_ids ON p1.id = valid_ids.id
+WHERE valid_ids.id IS NULL;
