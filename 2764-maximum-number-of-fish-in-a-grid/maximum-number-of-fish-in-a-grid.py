e5991060ecb11a464b5a18102bb0df78ab46d842
Nicholas <91217091+Stygian84@users.noreply.github.com>
2025-01-28 14:25:48 +0800

    Time: 175 ms (5.12%) | Memory: 18 MB (49.02%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+
+        n = len(grid)
+        m = len(grid[0])
+        res = 0
+
+        def bfs(start):
+            nonlocal res
+            queue = deque([start])
+            total = 0
+            seen = set()
+            seen.add(start)
+
+            while queue:
+                x,y = queue.popleft()
+                total += grid[x][y]
+
+                for dx,dy in directions:
+                    nx,ny = x+dx,y+dy
+                    if 0<=nx<n and 0<=ny<m and (nx,ny) not in seen and grid[nx][ny]!=0:
+                        queue.append((nx,ny))
+                        seen.add((nx,ny))
+            res = max(res,total)
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]!=0:
+                    bfs((i,j))
+        
+        return res
