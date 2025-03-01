2b992ac3c028094ae44a6a5ce2edaaac33de9cc3
Nicholas <91217091+Stygian84@users.noreply.github.com>
2025-03-01 19:26:09 +0700

    Time: 3 ms (42.59%) | Memory: 57.1 MB (37.04%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.ts
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.ts
@@ -1,26 +1,28 @@
 function applyOperations(nums: number[]): number[] {
-    let res : number[] = [];
-    for(let i=0;i<nums.length;i++){
-        res.push(0)
-    }
+    
     let j=0;
     let n = nums.length
     for (let i=0;i<n-1;i++){
         if (nums[i]!=0){
             if (nums[i]==nums[i+1]){
-                res[j]=nums[i]*2
+                nums[j]=nums[i]*2
                 nums[i+1]=0
             }
             else{
-                res[j]=nums[i]
+                nums[j]=nums[i]
             }
             j+=1
         }
-        console.log(i,res)
+        
     }
     if (nums[n-1]!=0){
-        res[j]=nums[n-1]
+        nums[j]=nums[n-1]
+        j+=1
+    }
+    while(j<n){
+        nums[j]=0
+        j+=1
     }
     
-    return res
+    return nums
 };
